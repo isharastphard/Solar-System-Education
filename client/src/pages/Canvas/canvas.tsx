cb40ad4bd0f4f04968a9e7cc209f8376eb10d6cf
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2021-06-24 20:31:02 -0400

    have animation working for earth

--- client/src/pages/Canvas/canvas.tsx
+++ client/src/pages/Canvas/canvas.tsx
@@ -1,64 +1,55 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
+import earth from "./earth.png"
+interface CanvasProps{
+  width: number;
+  height: number;
+}
 
-const Canvas: React.FunctionComponent = (props) => {
-  const canvasRef = useRef(null);
+const Canvas =({width, height}: CanvasProps) => {
+  let canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  const draw = (ctx: any) => {
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
-    ctx.strokeStyle = "rgba(0, 153, 255, 0.4)";
-    ctx.save();
-    ctx.translate(150, 150);
-    // Earth
+  const draw = useCallback((context:any) =>{
+    context.globalCompositeOperation ='destination-over';
+    context.clearRect(0,0,width,height); // clear canvas
+
+    context.fillStyle='rgba(0,0,0,0.4)';
+    context.strokeStyle = 'rgba(0,153,255,0.4)';
+    context.save();
+    context.translate(width/2,height/2)
+
+    //Earth
     var time = new Date();
-    ctx.rotate(
-      ((2 * Math.PI) / 60) * time.getSeconds() +
-        ((2 * Math.PI) / 60000) * time.getMilliseconds()
-    );
-    ctx.translate(105, 0);
-    ctx.fillRect(0, -12, 40, 24); // Shadow
-    
-    make_base();
+    context.rotate(((2*Math.PI)/60)*time.getSeconds()+((2*Math.PI)/60000)*time.getMilliseconds());
+    context.translate(105,0);
+    //context.fillRect (0,-12,50,24);//shadow
+    const Earth = document.createElement('img')
+    Earth.src = earth;
+    context.drawImage(Earth, 0,-12, 50, 54)
+    context.restore();          
+    context.beginPath()
 
-    function make_base()
-    {
-      var base_image = new Image();
-      base_image.src = 'canvas/earth.png';
-      base_image.onload = function(){
-        ctx.drawImage(base_image, -12, -12);
+    context.arc(width/2,height/2,105,0,Math.PI*2,false);//orbit of earth
+    context.stroke();
+
+    window.requestAnimationFrame(() => draw(context));},[width,height]) 
+
+    
+  useEffect(() => {
+    if(canvasRef.current){
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
+      if(context){
+        draw(context);
       }
     }
-
-    // Moon
-    ctx.save();
-    ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-    ctx.translate(0, 28.5);
-    ctx.restore();
-    ctx.restore();
-
-    ctx.beginPath();
-    ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-    ctx.stroke();
+  },[draw]);
   
-  };
 
-  useEffect(() => {
-    const canvas: any = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    let animationFrameId: number;
-    const render = () => {
-      draw(ctx);
-      animationFrameId = window.requestAnimationFrame(render);
-    };
-    render();
-
-    return () => {
-      window.cancelAnimationFrame(animationFrameId);
-    };
-  }, [draw]);
-
-  return <canvas ref={canvasRef} {...props} />;
+  return <canvas ref = {canvasRef} height={height} width ={width}/>
 };
 
+Canvas.defaultProps = {
+  width: window.innerWidth,
+  height:window.innerHeight
+};
 export default Canvas;
