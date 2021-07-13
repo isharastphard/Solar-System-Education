b97814cc2c56fd7139ba34ab08175c7ce91aa9d4
Jethr0 <jetzbond@gmail.com>
2021-07-13 19:47:26 -0400

    Eventlisteners in progress2

--- client/src/components/Canvas/Planets.tsx
+++ client/src/components/Canvas/Planets.tsx
@@ -18,7 +18,7 @@ interface CanvasProps{
   height: number;
 }
 
-const Canvas =({width, height}: CanvasProps) => {
+const Canvas = ({ width, height }: CanvasProps) => {
   let canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   const [modalContent, setModalContent] = useState(
@@ -26,140 +26,161 @@ const Canvas =({width, height}: CanvasProps) => {
   );
   const [showModal, setShowModal] = useState(false);
 
-  function makeOrbit(context:any, x:number){
+  function makeOrbit(context: any, x: number) {
     //context.beginPath();
-    const Circle = new Path2D()
-    Circle.arc(width/2,height/2,x,0,Math.PI*2,false);
+    const Circle = new Path2D();
+    Circle.arc(width / 2, height / 2, x, 0, Math.PI * 2, false);
     context.stroke(Circle);
     //context.closePath();
-    return Circle
+    return Circle;
   }
-  function makeBody(context: any, pic:any, x:number, y:number, pwidth:number, pheight:number) {
-    const Body = document.createElement('img');
+  function makeBody(
+    context: any,
+    pic: any,
+    x: number,
+    y: number,
+    pwidth: number,
+    pheight: number
+  ) {
+    const Body = document.createElement("img");
     Body.src = pic;
     context.drawImage(Body, x, y, pwidth, pheight);
   }
 
-  const draw = useCallback((context:any, canvas:any) =>{
-    context.globalCompositeOperation ='destination-over';
-    context.clearRect(0,0,width,height); // clear canvas
-    //context.fillStyle='rgba(0,0,0,0.4)';
-    context.strokeStyle = 'rgba(0,153,255,0.4)';
-    context.save();
-    context.translate(width/2,(height+70)/2);
+  const draw = useCallback(
+    (context: any, canvas: any) => {
+      context.globalCompositeOperation = "destination-over";
+      context.clearRect(0, 0, width, height); // clear canvas
+      //context.fillStyle='rgba(0,0,0,0.4)';
+      context.strokeStyle = "rgba(0,153,255,0.4)";
+      context.save();
+      context.translate(width / 2, (height + 70) / 2);
 
-    //PLANETS
+      //PLANETS
 
-    var time = new Date();
-   // context.rotate(((2*Math.PI)/60)*time.getSeconds()+((2*Math.PI)/60000)*time.getMilliseconds());
-    context.translate(90,0);
+      var time = new Date();
+      // context.rotate(((2*Math.PI)/60)*time.getSeconds()+((2*Math.PI)/60000)*time.getMilliseconds());
+      context.translate(90, 0);
 
-    //Mercury
-    //context.fillRect (-100,83,40,40);//shadow
-    makeBody(context, mercury, -65, -40, 20, 20);
-    //Venus
-    makeBody(context, venus, -143, -78, 30, 30);
-    //Earth
-    makeBody(context, earth, -100, 83, 35, 35);
-    //Mars - Might need another PNG without a shadow
-    makeBody(context, mars, 32, -12, 25, 25);
-    //Jupiter
-    makeBody(context, jupiter, -325, 30, 80, 80);
-    //Saturn
-    makeBody(context, saturn, -330, -150, 60, 60);
-    //Uranus
-    makeBody(context, uranus, -220, 245, 33, 33);
-    //Neptune
-    makeBody(context, neptune, 50, -300, 35, 35);
-    
-    context.restore();
+      //Mercury
+      //context.fillRect (-100,83,40,40);//shadow
+      makeBody(context, mercury, -65, -40, 20, 20);
+      //Venus
+      makeBody(context, venus, -143, -78, 30, 30);
+      //Earth
+      makeBody(context, earth, -100, 83, 35, 35);
+      //Mars - Might need another PNG without a shadow
+      makeBody(context, mars, 32, -12, 25, 25);
+      //Jupiter
+      makeBody(context, jupiter, -325, 30, 80, 80);
+      //Saturn
+      makeBody(context, saturn, -330, -150, 60, 60);
+      //Uranus
+      makeBody(context, uranus, -220, 245, 33, 33);
+      //Neptune
+      makeBody(context, neptune, 50, -300, 35, 35);
 
-    context.save();
-    //Sun
-<<<<<<< Updated upstream
-    makeBody(context, sun, (width-80)/2, (height-80)/2, 80, 80);
-=======
-    const Sun = document.createElement('img')
-    Sun.src = sun;
-    let sunDimension:number = 80 * .85;
-    context.drawImage(Sun, (width-sunDimension)/2, (height-sunDimension + 70)/2, sunDimension, sunDimension);
->>>>>>> Stashed changes
+      context.restore();
 
-    //asteroid belt
-    const Belt = document.createElement('img');
-    Belt.src = belt;
-    let x:number = 400;
-    let y:number = 0; //trying to find a way to change animation speed
-    let z:number = y/10;
-<<<<<<< Updated upstream
-    context.translate(width/2, height/2);
-    context.rotate(((2*Math.PI)/60)*(time.getSeconds()-z)+((2*Math.PI)/60000)*(time.getMilliseconds()-y)); 
-=======
-    context.translate(width/2, (height+70)/2);
-    context.rotate(((2*Math.PI)/60)*(time.getSeconds()-y)+((2*Math.PI)/60000)*(time.getMilliseconds()-y)); 
->>>>>>> Stashed changes
-    context.drawImage(Belt, (width-x)/2-(width)/2, (height-x)/2-(height)/2, x, x);
-    context.translate(-(width)/2, -(height)/2);
+      context.save();
+      //Sun
+      const Sun = document.createElement("img");
+      Sun.src = sun;
+      let sunDimension: number = 80 * 0.85;
+      context.drawImage(
+        Sun,
+        (width - sunDimension) / 2,
+        (height - sunDimension + 70) / 2,
+        sunDimension,
+        sunDimension
+      );
 
-    context.restore();
-<<<<<<< Updated upstream
+      //asteroid belt
+      const Belt = document.createElement("img");
+      Belt.src = belt;
+      let x: number = 400 * .85;
+      let y: number = 0; //trying to find a way to change animation speed
+      let z: number = y / 10;
+      context.translate(width / 2, (height + 70) / 2);
+      context.rotate(
+        ((2 * Math.PI) / 60) * (time.getSeconds() - y) +
+          ((2 * Math.PI) / 60000) * (time.getMilliseconds() - y)
+      );
+      context.drawImage(
+        Belt,
+        (width - x) / 2 - width / 2,
+        (height - x) / 2 - height / 2,
+        x,
+        x
+      );
+      context.translate(-width / 2, -height / 2);
 
-=======
-    context.save();
-    context.scale(.85,.85);
-    context.translate(110 + 25,57 + 50);
->>>>>>> Stashed changes
-    //ORBITS
-    //Mercury orbit
-    var MercuryO = makeOrbit(context, 45);
-    //Venus orbit
-    var VenusO = makeOrbit(context, 75);
-    //Earth orbit
-    var EarthO = makeOrbit(context, 105);
-    //Mars orbit
-    var MarsO = makeOrbit(context, 135);
-    //Jupiter orbit
-<<<<<<< Updated upstream
-    makeOrbit(context,205);
-=======
-    var JupiterO = makeOrbit(context, 205);
->>>>>>> Stashed changes
-    //Saturn orbit
-    var SaturnO = makeOrbit(context, 245);
-    //Uranus orbit
-    var UranusO = makeOrbit(context, 285);
-    //Neptune orbit
-    var NeptuneO = makeOrbit(context,325);
+      context.restore();
+      context.save();
+      context.scale(0.85, 0.85);
+      context.translate(110 + 25, 57 + 50);
+      //ORBITS
+      //Mercury orbit
+      var MercuryO = makeOrbit(context, 45);
+      //Venus orbit
+      var VenusO = makeOrbit(context, 75);
+      //Earth orbit
+      var EarthO = makeOrbit(context, 105);
+      //Mars orbit
+      var MarsO = makeOrbit(context, 135);
+      //Jupiter orbit
+      var JupiterO = makeOrbit(context, 205);
+      //Saturn orbit
+      var SaturnO = makeOrbit(context, 245);
+      //Uranus orbit
+      var UranusO = makeOrbit(context, 285);
+      //Neptune orbit
+      var NeptuneO = makeOrbit(context, 325);
 
-<<<<<<< Updated upstream
-    window.requestAnimationFrame(() => draw(context));},[width,height]) 
-=======
-    canvas.addEventListener('click', function(event:any) {
-      if(context.isPointInPath(MercuryO, event.offsetX, event.offsetY)){
-        setModalContent(<PlanetsInfo name = {'Mercury'}/>);
-        setShowModal(true);
-      }
-    })
-    
-    context.restore();
+      canvas.addEventListener("click", function (event: any) {
+        if (context.isPointInPath(MercuryO, event.offsetX, event.offsetY)) {
+          setModalContent(<PlanetsInfo name={"Mercury"} />);
+          setShowModal(true);
+        }
+      });
 
+      context.restore();
 
-    window.requestAnimationFrame(() => draw(context, canvas));},[width,height]) 
->>>>>>> Stashed changes
+      window.requestAnimationFrame(() => draw(context, canvas));
+    },
+    [width, height]
+  );
 
-    
   useEffect(() => {
-    if(canvasRef.current){
+    if (canvasRef.current) {
       const canvas = canvasRef.current;
-      const context = canvas.getContext('2d');
-      if(context){
+      const context = canvas.getContext("2d");
+      if (context) {
         draw(context, canvas);
       }
     }
-  },[draw]);
-  
+  }, [draw]);
 
-  return <canvas ref = {canvasRef} height={height} width ={width} style = {{position:'absolute', zIndex:50}}/>
+  return (
+    <canvas
+      ref={canvasRef}
+      height={height}
+      width={width}
+      style={{
+        padding: 0,
+        margin: "auto",
+        display: "block",
+        width: width,
+        height: height,
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 50,
+      }}
+    />
+  );
 };
 
 Canvas.defaultProps = {
