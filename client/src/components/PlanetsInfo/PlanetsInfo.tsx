449925cc7183c0fd8a87034a1a280bacc3191eae
Jethr0 <jetzbond@gmail.com>
2021-07-14 15:28:59 -0400

    json hooks

--- client/src/components/PlanetsInfo/PlanetsInfo.tsx
+++ client/src/components/PlanetsInfo/PlanetsInfo.tsx
@@ -1,18 +1,38 @@
 import React, { useEffect, useState } from "react";
 
 function PlanetsInfo(props: any) {
-    const [info, setinfo] = useState('')
-  useEffect( () => {
-    const fetchPlanets = async() =>{
-        //await fetch(`/planets/${props.name}`)
-        setinfo('planets info')
-    };
-    fetchPlanets();
-  }, []);
+  const [data , setData]=useState<string[]>([])
+
+  const getData=()=>{
+    fetch('quiz.json'
+    ,{
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        const filtered= myJson.filter(function(n:any){
+            console.log('\"'+n.subject+'\" ');
+            console.log('\"' + props.englishName.toLowerCase()+'\"')
+            return n.subject === props.englishName.toLowerCase()
+        })
+        console.log(filtered);
+        setData(filtered);
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+
+  const planet: any = [data];
+  console.log(planet.englishName);
 
   return (
     <div style={{ color: "Navy" }}> This is {props.name} 
-    <div> this is {props.name} info: {info}</div>
+    <div> this is {props.name} info: {planet[1]}</div>
     </div>
   )
 }
