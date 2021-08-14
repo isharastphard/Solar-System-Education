818d693338c19d920bc3857ccd44253f54d9b9b3
Ishara <isharastphard@gmail.com>
2021-08-13 22:42:33 -0400

    routes potentially finished

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, send_from_directory
+from flask import Flask, json, send_from_directory, request, jsonify
+from flask.templating import render_template
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
+from  pymongo import MongoClient
 
 app = Flask(__name__, static_url_path='', static_folder='Solar-System-Education\client\build')
 CORS(app) #comment this on deployment
 api = Api(app)
+
+client = MongoClient("mongodb+srv://Ishara:Heater287@capstone.tbgw0.mongodb.net/Capstone?retryWrites=true&w=majority")
+db = client.Capstone
+planet_blurb = db.planet_descriptions
+quiz_questions = db.quizzes
+planet_stats = db.planets_info
+
+@app.route("/Planets", methods = ['GET'])
+def getplanetinfo():
+    planets = (planet_stats.find())
+    return jsonify(planets)
+
+@app.route("/Quiz", methods = ["GET"])
+def getquizquestions():
+    quizzes = (quiz_questions.find())
+    return jsonify(quizzes)
+
+@app.route("/Planets", methods = ["GET"])
+def getplanet_descriptions():
+    descriptions = (planet_blurb.find())
+    return jsonify(descriptions)
+
+
 
 @app.route("/", defaults={'path':''})
 def serve(path):
