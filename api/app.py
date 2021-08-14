91a7e636bcd13e2f4ae14fcbc8e10d51db8d3bec
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2021-08-14 01:15:42 -0400

    fixed json format for planet_info

--- api/app.py
+++ api/app.py
@@ -2,19 +2,20 @@ from flask import Flask, json, send_from_directory, request, jsonify
 from flask.templating import render_template
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
-from api.HelloApiHandler import HelloApiHandler
 from  pymongo import MongoClient
 
 app = Flask(__name__, static_url_path='', static_folder='Solar-System-Education\client\build')
 CORS(app) #comment this on deployment
 api = Api(app)
 
+#Database
 client = MongoClient("mongodb+srv://Ishara:Heater287@capstone.tbgw0.mongodb.net/Capstone?retryWrites=true&w=majority")
 db = client.Capstone
 planet_blurb = db.planet_descriptions
 quiz_questions = db.quizzes
 planet_stats = db.planets_info
 
+#routes
 @app.route("/Planets", methods = ['GET'])
 def getplanetinfo():
     planets = (planet_stats.find())
@@ -25,7 +26,7 @@ def getquizquestions():
     quizzes = (quiz_questions.find())
     return jsonify(quizzes)
 
-@app.route("/Planets", methods = ["GET"])
+@app.route("/Planets/Descriptions", methods = ["GET"])
 def getplanet_descriptions():
     descriptions = (planet_blurb.find())
     return jsonify(descriptions)
@@ -36,4 +37,3 @@ def getplanet_descriptions():
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
-api.add_resource(HelloApiHandler, '/flask/hello')
