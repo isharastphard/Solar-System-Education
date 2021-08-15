fce1caf6a11b38afaabdb1d849e1d7fe1ea900ea
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2021-08-14 23:29:09 -0400

    worked with ishara to fix routes

--- api/app.py
+++ api/app.py
@@ -1,39 +1,55 @@
 from flask import Flask, json, send_from_directory, request, jsonify
 from flask.templating import render_template
 from flask_restful import Api, Resource, reqparse
+from bson.json_util import dumps
+import json 
+from bson import json_util
 from flask_cors import CORS #comment this on deployment
 from  pymongo import MongoClient
 
-app = Flask(__name__, static_url_path='', static_folder='Solar-System-Education\client\build')
+app = Flask(__name__)
+
 CORS(app) #comment this on deployment
 api = Api(app)
 
-#Database
+#Database connection
 client = MongoClient("mongodb+srv://Ishara:Heater287@capstone.tbgw0.mongodb.net/Capstone?retryWrites=true&w=majority")
-db = client.Capstone
-planet_blurb = db.planet_descriptions
-quiz_questions = db.quizzes
-planet_stats = db.planets_info
+db = client["Capstone"]
+planet_blurb = db["planet_descriptions"]
+quiz_questions = db["quiz_questions"]
+planet_stats = db["planet_info"]
+
 
 #routes
 @app.route("/Planets", methods = ['GET'])
 def getplanetinfo():
-    planets = (planet_stats.find())
-    return jsonify(planets)
+    planets = planet_stats.find()
+    response = []
+    for doc in planets:
+        doc['_id'] = str(doc['_id'])
+        response.append(doc)
+    return json.dumps(response)
+  
 
 @app.route("/Quiz", methods = ["GET"])
 def getquizquestions():
-    quizzes = (quiz_questions.find())
-    return jsonify(quizzes)
+    quizzes = quiz_questions.find()
+    response = []
+    for doc in quizzes:
+        doc['_id'] = str(doc['_id'])
+        response.append(doc)
+    return json.dumps(response)
 
-@app.route("/Planets/Descriptions", methods = ["GET"])
+
+@app.route("/PlanetsDescriptions", methods = ["GET"])
 def getplanet_descriptions():
-    descriptions = (planet_blurb.find())
-    return jsonify(descriptions)
+    descriptions = planet_blurb.find()
+    response = []
+    for doc in descriptions:
+        doc['_id'] = str(doc['_id'])
+        response.append(doc)
+    return json.dumps(response)
 
 
-
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
-
+if __name__ == "__main__":
+    app.run(debug=True)
