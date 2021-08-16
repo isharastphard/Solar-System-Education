31af61ec44d51956e4ef0445514387d5bd78190b
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2021-08-16 16:54:30 -0400

    last minute touches

---
+++ client/src/components/ClickHere/clickHere.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useRef } from "react";
+function clickHere() {
+  return (
+    <div style={{ color: "#fec604" }}>
+      <h1>Welcome to Our Project!</h1>
+      <p>The Sun is the center of our Solar System</p>
+      <div>You can zoom in and out with your mouse wheel!</div>
+      <br></br>
+      <div> You can also pan by holding left mouse click </div>
+      <br></br>
+      <h2>ENJOY!</h2>
+    </div>
+  );
+}
+
+export default clickHere;
